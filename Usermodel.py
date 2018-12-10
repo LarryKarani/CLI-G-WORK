1fc07a24f49894c26e8c8054b600752d7a9d7d57
SherylWaga <sherylwaga@gmail.com>
2018-12-10 13:11:50 +0300

    [Feature #162540496]Finish create a comment feature

--- Usermodel.py
+++ Usermodel.py
@@ -20,3 +20,22 @@ class Users():
         user['name']=self.username
         user['password']=self.password
         Users.counter +=1
+
+class Comments(Users):
+     count = 1
+     def __init__(self,comment,author):
+         self.comment = comment
+         self.author = author
+         self.user_id = self.checkuser(author)
+
+     def create_comment(self):
+         comment={}
+         comment['id'] = self.id
+         comment['description'] = self.comment
+         comment['author'] = self.author
+         comment['user_id'] = user_id
+         Comments.count +=1
+         
+
+
+        
