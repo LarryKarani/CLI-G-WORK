8f31c3e5fd6ac4310de00b53923ab9a8b7d66642
bafiam <stevebafiam@gmail.com>
2018-12-10 12:59:28 +0300

    [finished #162539075]create user logout

--- Usermodel.py
+++ Usermodel.py
@@ -1,12 +1,14 @@
 from datetime import datetime
+
 users = []
 comments = []
 replies = []
 
+
 class Users():
     counter = 1
-    
-    def __init__(self,username,password,isAdmin,isModerator):
+
+    def __init__(self, username, password, isAdmin, isModerator):
         self.id = self.counter
         self.createdAt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self.username = username
@@ -15,8 +17,12 @@ class Users():
         self.isModerator = False
 
     def user_signup(self):
-        user= {}
-        user['id']=self.id
-        user['name']=self.username
-        user['password']=self.password
-        Users.counter +=1
+        user = {}
+        user['id'] = self.id
+        user['name'] = self.username
+        user['password'] = self.password
+        Users.counter += 1
+
+    def log_out_user(self):
+        user_logout = False
+        return {"message": "successfully logged out"}
