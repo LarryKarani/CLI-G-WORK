0c4859226305debcfd4549f4b2e2467acf09779d
gkarumba <gachegua@gmail.com>
2018-12-10 15:08:59 +0300

    [finishes #162540538] Adds class Admin and Moderator

--- Usermodel.py
+++ Usermodel.py
@@ -20,3 +20,33 @@ class Users():
         user['name']=self.username
         user['password']=self.password
         Users.counter +=1
+
+class Moderator(Comments):
+    def delete_comment(self,id):
+        user = Comments.self.check_comment(id)
+        if user:
+            comments.remove(user)
+        return 'Comment deleted successfully'
+
+
+    def edit_single_comment(self,id):
+        user = Comments.self.check_user(id)
+        if user:
+            com = Comments.self.check_comment(id)
+            if com: 
+                com.comment = 'comment'
+                return 'edited successfully'
+                  
+              
+class Admin(Moderator):
+    def delete_comment(self,id):
+        user = Comments.self.check_comment(id)
+        if user:
+            comments.remove(user)
+        return 'Comment deleted successfully'
+
+    def edit_comment(self,id):
+        com = Comments.self.check_comment(id)
+        if com: 
+            com.comment = 'comment'
+            return 'edited successfully'
